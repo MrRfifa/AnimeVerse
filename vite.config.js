5f35ca4c4132d89785585da2567d60db1f02deb5
Anouar Rfifa <anouarrafifa99@gmail.com>
2023-07-19 12:53:02 +0100

    modified vite.config.js added env

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/AnimeVerse/", // Replace "AnimeVerse" with your actual GitHub repository name
+  define: {
+    // Expose environment variables to your application
+    "process.env.VITE_APP_RAPID_API_KEY": JSON.stringify(
+      import.meta.env.VITE_APP_RAPID_API_KEY
+    ),
+  },
 });
